e5fd8267726944a724911c770243eeb2b076c798
Jin Kato <jin.kato@starcomww.com>
2015-11-02 18:07:34 -0500

    init

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Jin Kato on 11/2/15.
+//  Copyright © 2015 Jin Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
