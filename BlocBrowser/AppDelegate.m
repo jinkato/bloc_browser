e15ca9f8b6fd9c9188d4242923186c3b9ca69a95
Jin Kato <jin.kato@starcomww.com>
2015-11-08 15:59:07 -0500

    added code from the tutorial

--- BlocBrowser/AppDelegate.m
+++ BlocBrowser/AppDelegate.m
@@ -7,16 +7,21 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
-
 @end
 
 @implementation AppDelegate
 
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
     return YES;
 }
 
