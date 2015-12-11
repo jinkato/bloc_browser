28ccd3dbe03bc1491caa4d21feb2d6e5b34a6689
Jin Kato <jin.kato@starcomww.com>
2015-12-11 16:27:07 -0500

    added code from tutorial

---
+++ BlocBrowser/AwesomeFloatingToolbar.h
@@ -0,0 +1,31 @@
+//
+//  AwesomeFloatingToolbar.h
+//  BlocBrowser
+//
+//  Created by Jin Kato on 11/29/15.
+//  Copyright © 2015 Jin Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AwesomeFloatingToolbar;
+
+@protocol AwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+- (void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title;
+
+@end
+
+
+
+@interface AwesomeFloatingToolbar : UIView
+
+- (instancetype) initWithFourTitles:(NSArray *)titles;
+
+- (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title;
+
+@property (nonatomic, weak) id <AwesomeFloatingToolbarDelegate> delegate;
+
+@end
---
+++ BlocBrowser/UIView+AwesomeFloatingToolbar.m
@@ -0,0 +1,13 @@
+//
+//  UIView+AwesomeFloatingToolbar.m
+//  BlocBrowser
+//
+//  Created by Jin Kato on 11/28/15.
+//  Copyright © 2015 Jin Kato. All rights reserved.
+//
+
+#import "UIView+AwesomeFloatingToolbar.h"
+
+@implementation UIView (AwesomeFloatingToolbar)
+
+@end
